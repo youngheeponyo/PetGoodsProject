adc68276223dbc36207f9f9d441332e68507b70f
user <user@DESKTOP-Q5EUN92>
2023-11-14 23:53:03 +0900

    [서영희]장바구니

--- src/main/java/com/yedamMiddle/cart/web/MyCartControl.java
+++ src/main/java/com/yedamMiddle/cart/web/MyCartControl.java
@@ -1,41 +1,34 @@
 package com.yedamMiddle.cart.web;
 
-
-
+import java.io.Console;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedamMiddle.cart.service.MyCartService;
-import com.yedamMiddle.cart.service.MyCartVO;
 import com.yedamMiddle.cart.serviceImpl.MyCartServiceImpl;
 import com.yedamMiddle.common.Command;
+import com.yedamMiddle.common.service.CartJoinVO;
 
 public class MyCartControl implements Command {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
-		String path = "cart/myCart.tiles";
-		
 		String uno = req.getParameter("uno");
 		
-//		
-//		MyCartService svc = new MyCartServiceImpl();
-//		MyCartVO vo = svc.myCartList(Integer.parseInt(uno));
-//		req.setAttribute("uno", vo);
-//		System.out.println(vo);
+		MyCartService svc = new MyCartServiceImpl();
+		CartJoinVO vo = svc.myCartList(Integer.parseInt(uno));
+		req.setAttribute("uno",vo);
 		
 		try {
-			req.getRequestDispatcher(path).forward(req, resp);
+			req.getRequestDispatcher("cart/myCart.tiles").forward(req, resp);
 		} catch (ServletException | IOException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
 		
 		
 		
