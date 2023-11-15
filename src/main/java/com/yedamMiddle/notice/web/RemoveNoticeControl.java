105aa120128ee517d248eceb6152863d617ecd53
Amin2-hub <wnstjd05@Naver.com>
2023-11-15 22:17:30 +0900

    [김준성]
    충돌해결완료

--- src/main/java/com/yedamMiddle/notice/web/RemoveNoticeControl.java
+++ src/main/java/com/yedamMiddle/notice/web/RemoveNoticeControl.java
@@ -1,16 +1,30 @@
 package com.yedamMiddle.notice.web;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedamMiddle.common.Command;
+import com.yedamMiddle.notice.service.NoticeService;
+import com.yedamMiddle.notice.serviceImpl.NoticeServiceImpl;
 
 public class RemoveNoticeControl implements Command {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
+		String nno = req.getParameter("nno");
+		int no = Integer.parseInt(nno);
 		
+		NoticeService svc = new NoticeServiceImpl();
+		
+		if(svc.removeNotice(no)) {
+			try {
+				resp.sendRedirect("noticeList.do");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
