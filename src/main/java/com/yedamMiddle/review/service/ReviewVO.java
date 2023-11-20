47caad43e2bd0a3dd21be899a5e2c8e7beede075
admin <admin@DESKTOP-3INP67K>
2023-11-20 14:18:56 +0900

    [김은별]-리뷰작업중

---
+++ src/main/java/com/yedamMiddle/review/service/ReviewVO.java
@@ -0,0 +1,16 @@
+package com.yedamMiddle.review.service;
+
+import java.util.Date;
+
+public class ReviewVO {
+	private int reviewNo;
+	private int merUid;
+	private String content;
+	private int starCnt;
+	private Date reviewDate;
+	private int reviewLikeCnt;
+	private int productNo;
+	private int UserNo;
+	private String reviewImage;
+	
+}
