27001351c6f0f6395889d474adbf831c381b5265
admin <admin@DESKTOP-3INP67K>
2023-11-20 09:23:30 +0900

    [김은별]-frontController에 내쿠폰 확인창 추가

---
+++ src/main/java/com/yedamMiddle/coupon/web/MyCouponListForm.java
@@ -0,0 +1,16 @@
+package com.yedamMiddle.coupon.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.yedamMiddle.common.Command;
+
+public class MyCouponListForm implements Command {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
